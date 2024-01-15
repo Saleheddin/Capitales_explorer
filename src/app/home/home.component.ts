bd4b454b3ff59c85304c1e2de429bd7fac78aaf7
Saleh eddine EL FDALI <elfdali31@gmail.com>
2024-01-15 02:49:24 +0100

    [user location] : display user location if authorised

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,26 @@ import { CapitalesService } from '../capitales.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  constructor(private capitalesService: CapitalesService) {
-    
-   }
-
-  ngOnInit(): void { 
-    this.markerPositions = this.capitalesService.getCities(); 
+  constructor(private capitalesService: CapitalesService) { }
+  ownLocation: any;
+  ngOnInit(): void {
+    this.markerPositions = this.capitalesService.getCities();
+    this.ownLocation = this.getUserLocation();
     this.capitalesService.setCurrentComponent(0);
-    console.log("currentcomponent: home" );
+    console.log("currentcomponent: home");
     console.log(this.capitalesService.currentComponent);
+  }
+  getUserLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+          this.ownLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+          console.log('Location accessed',this.ownLocation);
+      },
+      (error) => {
+          console.log('User did not allow location access');
+      },
+      { timeout: 10000 }
+  );
   }
   display: any;
   center: google.maps.LatLngLiteral = {
@@ -26,6 +37,10 @@ export class HomeComponent {
   };
   zoom = 4;
   selectedMarkerPosition: any = null;
+  userMarkerIcon: any = {
+    url: 'assets/User.png',
+    scaledSize: { width: 30, height: 30 },
+  };
 
   @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
 
@@ -35,15 +50,7 @@ export class HomeComponent {
   move(event: google.maps.MapMouseEvent) {
     if (event.latLng != null) this.display = event.latLng.toJSON();
   }
-  markerPositions :any;
-  // markerPositions = [
-  //   { lat: 40.416775, lng: -3.703790, name: 'Madrid', population: 3223334 },   // Madrid, Spain
-  //   { lat: 48.856613, lng: 2.352222, name: 'Paris', population: 2140526 },     // Paris, France
-  //   { lat: 38.736946, lng: -9.142685, name: 'Lisbon', population: 505526 },    // Lisbon, Portugal
-  //   { lat: 52.520008, lng: 13.404954, name: 'Berlin', population: 3748148 },   // Berlin, Germany
-  //   { lat: 50.850346, lng: 4.351721, name: 'Brussels', population: 185103 },   // Brussels, Belgium
-  //   { lat: 52.367573, lng: 4.904138, name: 'Amsterdam', population: 821752 }   // Amsterdam, Netherlands
-  // ];
+  markerPositions: any;
 
   openInfoWindow(marker: MapMarker, markerPosition: any) {
     this.selectedMarkerPosition = markerPosition;
